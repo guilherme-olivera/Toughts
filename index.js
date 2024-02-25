c108e55f28d86a4b25e1c9667141ef0e03c04798
Guilherme <guilherme.oliveirasantos@hotmail.com>
2024-02-25 14:27:39 -0300

    estrutura do projeto

--- index.js
+++ index.js
@@ -8,7 +8,54 @@ const app = express();
 
 const conn = require('./db/conn');
 
+//template engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
+//reeber rsposta do body
+app.use(
+    express.urlencoded({
+        extended:true
+    })
+)
+
+app.use(express.json())
+
+//session midleware, mantem a seção 
+app.use(
+    session({
+        name:"session",
+        secret:"nosso_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'session'),
+        }),
+        cookie:{
+            secure:false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    }),
+)
+
+//flash messages
+app.use(flash())
+
+//public path
+app.use(express.static('public'))
+
+//set session to res
+app.use((req, res, next) =>{
+
+ if(req.session.userid){
+    res.locals.session = req.session
+ }
+ next();
+
+})
 
 conn .sync()
      .then(() => {
