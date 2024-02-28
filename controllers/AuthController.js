797e2767389bb4f76c684d13e48bd313171c0998
Guilherme <guilherme.oliveirasantos@hotmail.com>
2024-02-28 17:22:10 -0300

    Confirmpassword

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const user = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class Authcontroller{
     static login(req, res) {
         res.render('auth/login')
@@ -5,6 +9,20 @@ module.exports = class Authcontroller{
 
     static register(req, res){
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res){
+        const{name, email, password, confirmpasseword} = req. body
+
+        // password macth validation
+        if(password != confirmpasseword){
+            //mensagem para o Front
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
+
     }
 }
 
